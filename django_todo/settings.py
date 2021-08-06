6882664d62e78c17c5363324f4dff01c1bf68e70
AndyMc3000 <andypatmcd@gmail.com>
2021-08-06 14:14:22 +0000

    Create new postrgres database on Heroku and connect it to Django app on GitPod

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://bnxyxnztgsfnby:337324ef392c8d36db1a9de8e51b68509d045d03c35a3913115c6402687d0ebb@ec2-52-0-67-144.compute-1.amazonaws.com:5432/d7d63amrr5u686')
 }
 
 
